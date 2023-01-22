c15ee7c9bc0ee21654a34b6bc34edab81517aaec
advanc3dUA <fash1k@gmail.com>
2023-01-22 12:08:14 +0100

    Initial commit

---
+++ WeatherApp/World.swift
@@ -0,0 +1,23 @@
+//
+//  World.swift
+//  WeatherApp
+//
+//  Created by Ben Scheirman on 9/18/20.
+//
+
+import Foundation
+import WeatherKit
+
+struct World {
+    var appState: AppState
+    var citiesStore: CitiesStore
+    var weatherAPI: OpenWeatherMapAPIClient
+    var weatherManager: WeatherManager
+}
+
+var Current = World(
+    appState: AppState(),
+    citiesStore: CitiesStore.load(),
+    weatherAPI: OpenWeatherMapAPIClient(),
+    weatherManager: WeatherManager()
+)
